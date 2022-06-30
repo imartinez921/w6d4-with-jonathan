a04c2e1b51cb86d2bd0e10a712e076894323a0a1
App Academy Student <email@email.com>
2022-06-30 11:55:13 -0400

    Initial commit for art_share_api_project; create artists nand artworks tables and models

---
+++ art_share_api_project/app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render json: @users
+    end
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :id)
+    end
+
+    def create
+        # render json: params
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json:user
+        else
+            render json: user.errors.full_messages, status: unprocessable_entity
+        end
+    end
+
+    def show
+        # render json: params
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+
+    def update
+        # debugger
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to user_url
+    end
+end
