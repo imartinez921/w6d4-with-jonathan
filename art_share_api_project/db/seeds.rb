227807e58190d6921ce159ccebb558c0de8131e2
App Academy Student <email@email.com>
2022-06-30 13:46:47 -0400

    Seed database with users, artworks, artworkshares

--- art_share_api_project/db/seeds.rb
+++ art_share_api_project/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#starts at id 3
+User.create(username: 'Banksy')
+User.create(username: 'Leonardo')
+User.create(username: 'VanGogh')
+
+#starts at id 1
+Artwork.create(title: 'Girl with Red Balloon', image_url: 'redballoon', artist_id: 3)
+Artwork.create(title: 'Mona Lisa', image_url: 'monalisa', artist_id: 4)
+Artwork.create(title: 'Starry Night', image_url: 'starrynight', artist_id: 5)
+Artwork.create(title: 'No Ear', image_url: 'noear', artist_id: 5)
+
+ArtworkShare.create(artwork_id: 1, viewer_id: 1)
+ArtworkShare.create(artwork_id: 2, viewer_id: 1)
+ArtworkShare.create(artwork_id: 3, viewer_id: 1)
+ArtworkShare.create(artwork_id: 2, viewer_id: 2)
+ArtworkShare.create(artwork_id: 1, viewer_id: 2)
+ArtworkShare.create(artwork_id: 4, viewer_id: 2)
